6592cdbbfae5d3ba1264070c79f8c04aa44474a0
shaberz <shaberz.to@gmail.com>
2025-08-17 12:47:53 +0330

    banner

--- components/banner/home-banner.tsx
+++ components/banner/home-banner.tsx
@@ -3,8 +3,9 @@ import Title from "../layouts/title"
 
 const HomeBanner = () => {
   return (
-    <div>
+    <div className="flex items-center flex-col justify-center gap-5">
       <Title className="text-3xl md:text-4xl font-bold text-center uppercase">Our best collections</Title>
+      <p className="text-sm text-center text-lightColor/80 font-medium max-w-[480px]">Find everything you need to look and feel your best, and shop the latest men&apos;s fashion and life style products</p>
     </div>
   )
 }
--- components/layouts/header/index.tsx
+++ components/layouts/header/index.tsx
@@ -6,8 +6,12 @@ import Container from "../container";
 import MobileMenu from "./left-bar/mobile-menu";
 import SearchBar from "@/components/layouts/header/right-bar/search-bar";
 import CardIcon from "@/components/layouts/header/right-bar/card-icon";
+import { currentUser } from "@clerk/nextjs/server";
+import { ClerkLoaded, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
+import { ListOrdered } from "lucide-react";
 
-const Header = () => {
+const Header = async() => {
+  const user = await currentUser()
   return (
     <header className="border-b border-b-gray-200 py-5">
       <Container className="flex items-center justify-between gap-7 text-lightColor">
@@ -19,11 +23,26 @@ const Header = () => {
         <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
           <SearchBar />
           <CardIcon />
-          <Link href={"/login"}>
-            <Button className="bg-lightBg text-darkColor hover:text-white">
-              Login
-            </Button>
-          </Link>
+          <ClerkLoaded>
+            <SignedIn>
+              <Link href={'/orders'} className="group relative">
+                <ListOrdered className="w-5 h-5 group hover:text-darkColor hoverEffect"/>
+                <span className="absolute -top-1 -right-1 bg-darkColor text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex justify-center items-center">0</span>
+              </Link>
+              <UserButton />
+            </SignedIn>
+          {!user
+          &&
+          <SignInButton mode="modal">
+            <div>
+              <Button className="bg-lightBg text-darkColor hover:text-white">
+                Login
+              </Button>
+            </div>
+          </SignInButton>
+          }
+          </ClerkLoaded>
+
         </div>
       </Container>
     </header>
