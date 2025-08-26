8ef1104d3c5e7e71e20a5a2f556f20305b963666
shaberz <shaberz.to@gmail.com>
2025-08-26 17:01:48 +0330

    sanity & studio

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
 import Header from "@/components/layouts/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import localFont from "next/font/local"
 
 
 const raleway = localFont({
-  src: "../public/fonts/raleway.woff2",
+  src: "../../public/fonts/raleway.woff2",
   variable: "--font-raleway",
   weight: "100 900",
 })
