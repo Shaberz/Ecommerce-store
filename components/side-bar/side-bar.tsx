863160aeed5c1347315d76bf4cc47ecbe3a0475d
shaberz <shaberz.to@gmail.com>
2025-08-26 12:58:53 +0330

    click handler

--- components/side-bar/side-bar.tsx
+++ components/side-bar/side-bar.tsx
@@ -6,6 +6,7 @@ import { X } from "lucide-react";
 import Link from "next/link";
 import { headerData } from "@/constants";
 import SocialMedia from "../social-media/social-media";
+import { useOutsideClick } from "@/hooks/user-outside-click";
 
 
 interface SideProps {
@@ -16,12 +17,15 @@ interface SideProps {
 const SideBar:FC<SideProps> = ({isOpen, onClose}) => {
   const pathName  = usePathname()
   const router = useRouter()
+  const sidebarRef =useOutsideClick<HTMLDivElement>(onClose)
+
   return (
-    <div className={`fixed inset-y-0 left-0 z-50 bg-darkColor/50 shadow-xl hoverEffect w-full ${isOpen ? 'translate-x-0':'-translate-x-full'}`}>
+    <div className={`fixed inset-y-0 left-0 z-50 bg-darkColor/50 shadow-xl hoverEffect cursor-auto w-full ${isOpen ? 'translate-x-0':'-translate-x-full'}`}>
       <motion.div
       initial={{opacity: 0}}
       animate={{opacity: 1}}
       transition={{delay: 0.7, duration: 0.5}}
+      ref={sidebarRef}
       className="min-w-72 max-w-96 bg-darkColor text-white/90 h-lvh p-10 border-r border-r-white flex flex-col gap-5">
         <div className="flex items-center justify-between">
           <CustomIcon iconName="logo" onClick={() => {
@@ -36,7 +40,7 @@ const SideBar:FC<SideProps> = ({isOpen, onClose}) => {
             key={items?.title}
             href={items?.href}
             onClick={onClose}
-            className={`hover:text-white/40 hoverEffect relative group ${pathName === items.href &&'text-white'}`}
+            className={`hover:text-white/40 w-20 hoverEffect relative group ${pathName === items.href &&'text-white'}`}
           >
             {items?.title}
           </Link>
